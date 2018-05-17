2f44d0e9faa97a53a84fa3d24ef53edfef8a68cd
HACKING-STATION\gunhaxxor <gunnar.oledal@gmail.com>
2018-05-17 08:51:55 +0200

    Trying to allow CORS (cross origin shit storm)

--- signaling-server.js
+++ signaling-server.js
@@ -19,7 +19,24 @@ console.log(
   'ROBOT_TOKEN = ' + ROBOT_TOKEN + '\nCLIENT_TOKEN = ' + CLIENT_TOKEN
 );
 
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization, Content-Length, X-Requested-With'
+  );
+
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
+};
+
 server = express()
+  .use(allowCrossDomain)
   // .use('/camera', (req, res) => {
   //   res.sendFile(path.join(__dirname, 'camera.html'));
   // })
